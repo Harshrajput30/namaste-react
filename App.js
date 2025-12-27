46bea244957f878318ded659bcf9dea8d607cbe7
Harsh Rajput <harshrajputt45@gmail.com>
2025-12-27 21:02:15 +0530

    Components

--- App.js
+++ App.js
@@ -1,17 +1,55 @@
-const parent  = React.createElement('div', {id: 'parent'},
-   [React.createElement('div', {id: 'child'},
-        [React.createElement('h1', {}, 'I am h1 tag'),
-        React.createElement('h1', {}, 'I am h2 tag')]
-),React.createElement('div', {id: 'child2'},
-        [React.createElement('h1', {}, 'I am h1 tag'),
-        React.createElement('h1', {}, 'I am h2 tag')]
-)
-]
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+//React.createElement => React element(object)=> HTML(DOM)
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  [
+    React.createElement(
+      'div',
+      { id: 'child', key: 'child1' },
+      [
+        React.createElement('h1', { key: 'h1-1' }, 'I am h1 tag'),
+        React.createElement('h2', { key: 'h2-1' }, 'I am h2 tag')
+      ]
+    ),
+    React.createElement(
+      'div',
+      { id: 'child2', key: 'child2' },
+      [
+        React.createElement('h1', { key: 'h1-2' }, 'I am h1 tag'),
+        React.createElement('h2', { key: 'h2-2' }, 'I am h2 tag')
+      ]
+    )
+  ]
 );
 
+//Using JSX
+// jsx => React.createElement => React element(object) => HTML(DOM)
+
+const jsxheading = <h1>This is from the jsx Heading</h1>
+
+
+// React functional component
+const Title = () => {
+  return <h1>This is from the Title Component</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h1>This is from the Functional Component</h1>
+    </div>
+  );
+};
+
+  
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<HeadingComponent />);
