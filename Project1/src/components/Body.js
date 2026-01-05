4ffe6376780b025e9958d5dce7bfad9405f79345
Harsh Rajput <harshrajputt45@gmail.com>
2026-01-05 19:20:20 +0530

    UseState Top Rated Restro Button

--- Project1/src/components/Body.js
+++ Project1/src/components/Body.js
@@ -1,14 +1,22 @@
 import RestaurantCard from './RestaurantCard';
 import FOOD_IMAGES from '../constants/foodimages';
-import restaurantList from '../constants/mockdata';  
+import restaurantList from '../constants/mockdata'; 
+import { useState } from 'react'; 
 
 
 const Body = () => {
+    const[listofRestro, setlistofRestro]= useState(restaurantList);
   return (
     <>
-    <div className='search'>Search</div>
+    <div className='filter'>
+        <button className='filter-btn' onClick={() => {
+             const filteredRestaurants = listofRestro.filter(restaurant => restaurant.data.avgRating > 4);
+             setlistofRestro(filteredRestaurants);
+        }}>
+            Top Rated Restaurants</button>
+        </div>
     <div className="restaurant-list">
-      {restaurantList.map((restaurant, index) => {
+      {listofRestro.map((restaurant,index) => {
         return (
           <RestaurantCard
             key={restaurant.data.id}
