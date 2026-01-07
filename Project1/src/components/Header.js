aeeee16b210a17463a77d9f4a230d007a047c213
Harsh Rajput <harshrajputt45@gmail.com>
2026-01-07 19:35:09 +0530

    Link

--- Project1/src/components/Header.js
+++ Project1/src/components/Header.js
@@ -1,5 +1,7 @@
 import logo from "url:./Logo.png";
 import { useState } from "react";
+import {Link} from "react-router-dom";
+
 const Header = () => {
     
     const [btnNameReact, setBtnNameReact] = useState("LogIn");
@@ -10,10 +12,10 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
+                    <li><Link to="/cart">Cart</Link></li>
                     <button className="login" onClick={() => {
                        btnNameReact==="LogIn"
                        ?setBtnNameReact("LogOut")
